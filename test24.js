163b4f711c762c933534cbd604d733453109407a
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2022-04-24 23:42:26 +0530

    Update test24.js

--- test24.js
+++ test24.js
@@ -6,31 +6,33 @@ const reviews = [
         job:"manager",
         img:"https://reqres.in/img/faces/7-image.jpg",
         text:"kus",
-{
-        id:1,
-        fname:"krishna",
+   {
+        id:2,
+        fname:"manish",
         name:"pate",
         job:"manager",
         img:"https://reqres.in/img/faces/7-image.jpg",
         text:"kus",
     },
     {
-        id:1,
-        fname:"krishna",
-        name:"pate",l
+        id:3,
+        fname:"nitib",
+        name:"pate",
         job:"manager",
         img:"https://reqres.in/img/faces/7-image.jpg",
         text:"kus",
     },
     {
-        id:1,
+        id:4,
         fname:"krishna",
-        name:"pate",l
+        name:"pate",
         job:"manager",
         img:"https://reqres.in/img/faces/7-image.jpg",
         text:"kus",
     },
-]const indexToBeDisplayed = 1
+];
+
+let indexToBeDisplayed = 0
 const currentElement = reviews[indexToBeDisplayed]
 console.log(currentElement)
 const imageElement = document.querySelector("#image")
@@ -39,8 +41,25 @@ const nameElement = document.querySelector(".name")
 const jobElement = document.querySelector(".job")
 const textElement = document.querySelector(".text")
 
-fnameElement.innerText = currentElement.fname
-nameElement.innerText = currentElement.name
-jobElement.innerText = currentElement.job
-textElement.innerText = currentElement.text
-imageElement.src = currentElement.img
+const nextbt =document.querySelector("#next")
+const prevbt = document.querySelector("#prev")
+
+nextbt.addEventListener('click', displayNext)
+prevbt.addEventListener('click', displayPrev)
+ function displayNext(){
+     console.log("nextbt")
+ }
+ function displayPrev(){
+     console.log("prevbt")
+ }
+function updates(krishna)
+{
+
+    fnameElement.innerText = krishna.fname
+    nameElement.innerText = krishna.name
+    jobElement.innerText = krishna.job
+    textElement.innerText = krishna.text
+    imageElement.src = krishna.img
+
+}
+updates(currentElement)
